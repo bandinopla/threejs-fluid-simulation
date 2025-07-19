61ee6d81224062ae0aef9d5eb09baec1a14d7f89
bandinopla <pablobandinopla@gmail.com>
2025-07-19 08:21:35 +0000

    show demo in webGL since the GPU impl is broken

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,2 @@
-//import "./main-webgl"; // Use this to test the webGL example version
-import "./main-web-gpu"; // Use this to test the webGPU example version
+import "./main-webgl"; // Use this to test the webGL example version
+//import "./main-web-gpu"; // Use this to test the webGPU example version
